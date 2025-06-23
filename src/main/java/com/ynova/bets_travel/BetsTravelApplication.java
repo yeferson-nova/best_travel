77313245811226c943a6fb83d73600621d3920d1
Yeferson Nova <yeferson_nova@outlook.com>
2025-06-23 17:15:37 -0500

    Primer commit

---
+++ src/main/java/com/ynova/bets_travel/BetsTravelApplication.java
@@ -0,0 +1,13 @@
+package com.ynova.bets_travel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetsTravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetsTravelApplication.class, args);
+	}
+
+}
